a3bb82061300482a05c0cc2c7e02cd5c8cf37535
wh0 <wh0@kakao.com>
2022-10-23 15:13:25 +0900

    no message

---
+++ Momentum/app.js
@@ -0,0 +1,13 @@
+alert("hi")
+
+const a = 10;
+const b = 2
+let myName = "ingyu";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello " + myName);
+
+myName = "YangInGyu";
+console.log("your new name is " + myName);
